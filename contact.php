341cfd41ca743a79348c774c3332c950e0c0d1fd
Pierre Perkov <pierreperkov.pp@gmail.com>
2019-12-19 11:48:38 +0100

    init project page contacts

---
+++ contact.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+      <html lang="fr">
+        <head>
+          <meta charset="UTF-8">
+          <title>Forum</title>
+          <link rel="stylesheet" href="style_section.css">
+        </head>
+        <body>
+          <div class="conteneur">
+            <?php
+            if(isset($_SESSION['pseudo']) AND isset($_SESSION['id']))
+            {  
+            include('header_auth.php');
+            }else
+            {
+                include('header.php');
+            }
+            ?>
+            <h2>Nous contacter</h2>
+            <table>
+                <tr>
+                    <td class="contact_mail">Par mail:</td>
+                    <td>leblogdelexpress@gmail.com</td>
+                </tr>
+            </table>
+        
+        
