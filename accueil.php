0ffa74421fe108b0a061f1de5e0ad6253273eb90
Pierre Perkov <pierreperkov.pp@gmail.com>
2019-12-19 11:44:41 +0100

    init project accueil

---
+++ accueil.php
@@ -0,0 +1,28 @@
+<?php
+include('header.php');
+?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Accueil</title>
+        <link rel="stylesheet" href="style_section.css">
+    </head>
+    <body>
+        <div class="conteneur">
+            <div class="msg">
+                <p>Le site est en construction!</p>
+                <p>Il y aura bientôt des articles sur cette page, vous pouvez vous rendre sur la page forum.</p>
+            </div>
+        </div> 
+    </body>
+</html>
+
+
+
+
+
+
+
+
+
