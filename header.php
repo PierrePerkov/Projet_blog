56ad65c2425ab6fe45fa91c8867e87d7fd6dff93
Pierre Perkov <pierreperkov.pp@gmail.com>
2020-01-03 10:12:32 +0100

    replace header.php file

--- header.php
+++ header.php
@@ -2,35 +2,35 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Forum</title>
-    <link rel="stylesheet" href="style_non_conn.css">
+    <title>header</title>
+    <link rel="stylesheet" href="style_not_conn.css">
 </head>
 <body>
     <header>
         <div class="menu">
             <nav>
                 <ul>
-                    <a href = "accueil.php"><li>ACCUEIL</li></a>
+                    <a href = "index.php"><li>ACCUEIL</li></a>
                     <a href = "forum.php"><li>FORUM</li></a>
                     <a href="contact.php"><li>CONTACT</li></a>           
                 </ul>
             </nav>
             <div class="connection">
                 <form method="post" action="login.php">
-                    <label for="pseudo">Pseudo</label>
-                    <input type="texte" name="pseudo" id="pseudo">
-                    <label for="motdepasse">Mot de passe</label>
-                    <input type="password" name="motdepasse" id="motdepasse">
-                    <button name="seconnecter">Se connecter</button>
+                    <label for="login">Pseudo</label>
+                    <input type="text" name="login" id="login">
+                    <label for="pass">Mot de passe</label>
+                    <input type="password" name="pass" id="pass">
+                    <button name="connect">Se connecter</button>
                     
                 </form>
-                <div class="inscription">            
-                    <a href="page_inscriptions.php"><button>S'inscrire</button></a>
+                <div class="registration">            
+                    <a href="registration_form.php"><button>S'inscrire</button></a>
                 </div>
             </div>
         </div>    
-            <div class="titre_logo">
-                <img src="images/logo_titre.png" alt="logo image">
+            <div class="title_logo">
+                <img src="pictures/logo_title.png" alt="logo">
                 <h1>Le blog de l'express</h1>
             </div>       
         </header>
