0ed73bd3c869d322bdbeb20d0ff85b097a6467d1
Pierre Perkov <pierreperkov.pp@gmail.com>
2020-01-03 10:13:45 +0100

    replace accueil.php file

---
+++ index.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Accueil</title>
+        <link rel="stylesheet" href="style_section.css">
+    </head>
+    <body>
+        <div class="container">
+            <?php include('header.php');?> 
+            <p class="msg">Le site est en construction!</p>
+            <p class="msg">Il y aura bientôt des articles sur cette page, vous pouvez vous rendre sur la page forum.</p>        
+        </div> 
+    </body>
+</html>
+
+
+           
+            
+
+
+
+
+
+
+
